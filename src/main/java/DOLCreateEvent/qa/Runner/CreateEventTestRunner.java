7e7e4db60c661082698b302cb2fe0c4b287f4dfa
spuri101090 <76819788+spuri101090@users.noreply.github.com>
2021-01-02 18:48:47 -0500

    Update CreateEventTestRunner.java

--- src/main/java/DOLCreateEvent/qa/Runner/CreateEventTestRunner.java
+++ src/main/java/DOLCreateEvent/qa/Runner/CreateEventTestRunner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 			monochrome = true, //display the console output in a proper readable format
 			strict = true, //it will check if any step is not defined in step definition file
 			dryRun = false,//to check the mapping is proper between feature file and step def file
-			tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
+			tags ={"@SmokeTest"}			
 			
 			)
 	
